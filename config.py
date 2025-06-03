c080002cfa0655aee174bb18303fbfe752670921
ULISESDLAPTOP\ulise <202211403@alu.comillas.edu>
2025-06-03 20:41:23 +0200

    Config

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "default-insecure-key")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    UPLOAD_FOLDER = os.environ.get("UPLOAD_FOLDER", "/var/uploads")
